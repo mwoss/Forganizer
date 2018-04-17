3ebc07ec3f122ea3f344eddfb3a936d0e357c573
Mateusz <mateusz14321@gmail.com>
2018-04-17 22:36:04 +0200

    Added indexing documents and timer to indexing directory

--- src/main/java/pl/edu/agh/ki/io/forganizer/IndexFiles.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/IndexFiles.java
@@ -2,15 +2,20 @@ package pl.edu.agh.ki.io.forganizer;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 
 public class IndexFiles {
@@ -49,6 +54,7 @@ public class IndexFiles {
             System.exit(1);
         }
 
+        Date start = new Date();
         try {
             System.out.println("Indexing to directory '" + indexPath + "'...");
 
@@ -64,13 +70,56 @@ public class IndexFiles {
 
             IndexWriter writer = new IndexWriter(dir, iwc);
 
-            //indexDocs(writer, docDir);
+            indexDocs(writer, docDir);
 
             writer.close();
+
+            Date end = new Date();
+            System.out.println(end.getTime() - start.getTime() + " total milliseconds");
 
         } catch (IOException e) {
             System.out.println(" caught a " + e.getClass() +
                     "\n with message: " + e.getMessage());
+        }
+    }
+
+    static void indexDocs(final IndexWriter writer, Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    try {
+                        indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+                    } catch (IOException ignore) {
+                        // don't index files that can't be read.
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } else {
+            indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+        }
+    }
+
+    static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
+        try (InputStream stream = Files.newInputStream(file)) {
+            // make a new, empty document
+            Document doc = new Document();
+
+            Field pathField = new StringField("path", file.toString(), Field.Store.YES);
+            doc.add(pathField);
+
+            doc.add(new LongPoint("modified", lastModified));
+
+            doc.add(new TextField("contents", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+
+            if (writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
+                System.out.println("adding " + file);
+                writer.addDocument(doc);
+            } else {
+                System.out.println("updating " + file);
+                writer.updateDocument(new Term("path", file.toString()), doc);
+            }
         }
     }
 }
