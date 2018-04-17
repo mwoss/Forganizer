847da98f324319576157a0d9cfa591bc6e283b48
Mateusz <mateusz14321@gmail.com>
2018-04-17 21:24:33 +0200

    Implemented Program Option parsing

--- src/main/java/pl/edu/agh/ki/io/forganizer/SearchFiles.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/SearchFiles.java
@@ -4,7 +4,49 @@ public class SearchFiles {
 
     private SearchFiles() {
     }
-    public static void main(String[] args){
 
+    public static void main(String[] args){
+        String usage =
+                "Usage:\tjava org.apache.lucene.demo.SearchFiles [-index dir] [-field f] [-repeat n] [-queries file] [-query string] [-raw] [-paging hitsPerPage]\n";
+        if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
+            System.out.println(usage);
+            System.exit(0);
+        }
+
+        String index = "index";
+        String field = "contents";
+        String queries = null;
+        int repeat = 0;
+        boolean raw = false;
+        String queryString = null;
+        int hitsPerPage = 10;
+
+        for (int i = 0; i < args.length; i++) {
+            if ("-index".equals(args[i])) {
+                index = args[i + 1];
+                i++;
+            } else if ("-field".equals(args[i])) {
+                field = args[i + 1];
+                i++;
+            } else if ("-queries".equals(args[i])) {
+                queries = args[i + 1];
+                i++;
+            } else if ("-query".equals(args[i])) {
+                queryString = args[i + 1];
+                i++;
+            } else if ("-repeat".equals(args[i])) {
+                repeat = Integer.parseInt(args[i + 1]);
+                i++;
+            } else if ("-raw".equals(args[i])) {
+                raw = true;
+            } else if ("-paging".equals(args[i])) {
+                hitsPerPage = Integer.parseInt(args[i + 1]);
+                if (hitsPerPage <= 0) {
+                    System.err.println("There must be at least 1 hit per page.");
+                    System.exit(1);
+                }
+                i++;
+            }
+        }
     }
 }
