5c6f4ee5eeb516a93c8c3201859533b8591677d1
mwoss <wos.mateusz16@gmail.com>
2018-04-17 22:11:22 +0200

    Added main app class

--- src/main/java/pl/edu/agh/ki/io/forganizer/application/Main.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/application/Main.java
@@ -1,4 +1,17 @@
 package pl.edu.agh.ki.io.forganizer.application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import pl.edu.agh.ki.io.forganizer.view.MainWindowView;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        MainWindowView mainWindowView = new MainWindowView(primaryStage);
+        mainWindowView.showStage();
+    }
 }
