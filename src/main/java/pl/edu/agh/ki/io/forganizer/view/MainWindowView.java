c23d8f50073ffc4644931e290e002968aa9145cf
mwoss <wos.mateusz16@gmail.com>
2018-04-17 21:41:24 +0200

    MainWindowView class update

---
+++ src/main/java/pl/edu/agh/ki/io/forganizer/view/MainWindowView.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.ki.io.forganizer.view;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainWindowView {
+    private final String TITLE = "Forganizer";
+    private final String FXML_FILE_NAME = "mainWindowView.fxml";
+
+    private Stage primaryStage;
+
+    public MainWindowView(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public void showStage(){
+        primaryStage.show();
+    }
+
+    private void setupStage() throws IOException {
+        primaryStage.setTitle(TITLE);
+        primaryStage.setScene(new Scene(getRoot()));
+    }
+
+    private Parent getRoot() throws IOException {
+        return FXMLLoader.load(getClass().getResource(FXML_FILE_NAME));
+    }
+}
--- src/main/java/pl/edu/agh/ki/io/forganizer/view/View.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.agh.ki.io.forganizer.view;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class View {
-
-}
