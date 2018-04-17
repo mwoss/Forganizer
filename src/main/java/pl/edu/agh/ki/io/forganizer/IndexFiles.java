b5081994b3ece3b5673391795781cc4d225573bc
Mateusz <mateusz14321@gmail.com>
2018-04-17 21:31:47 +0200

    Implemented Program Option parsing for indexing

--- src/main/java/pl/edu/agh/ki/io/forganizer/IndexFiles.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/IndexFiles.java
@@ -4,4 +4,25 @@ public class IndexFiles {
 
     private IndexFiles() {
     }
+
+    public static void main(String[] args) {
+        String usage = "java org.apache.lucene.demo.IndexFiles"
+                + " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
+                + "This indexes the documents in DOCS_PATH, creating a Lucene index"
+                + "in INDEX_PATH that can be searched with SearchFiles";
+        String indexPath = "index";
+        String docsPath = null;
+        boolean create = true;
+        for (int i = 0; i < args.length; i++) {
+            if ("-index".equals(args[i])) {
+                indexPath = args[i + 1];
+                i++;
+            } else if ("-docs".equals(args[i])) {
+                docsPath = args[i + 1];
+                i++;
+            } else if ("-update".equals(args[i])) {
+                create = false;
+            }
+        }
+    }
 }
