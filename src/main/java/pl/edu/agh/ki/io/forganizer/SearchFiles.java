0d87ee44007eda69c0823902ce0f9022ebc6cfb4
Mateusz <mateusz14321@gmail.com>
2018-04-18 00:48:00 +0200

    Added searching module

--- src/main/java/pl/edu/agh/ki/io/forganizer/SearchFiles.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/SearchFiles.java
@@ -1,11 +1,29 @@
 package pl.edu.agh.ki.io.forganizer;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+
 public class SearchFiles {
 
     private SearchFiles() {
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, ParseException {
         String usage =
                 "Usage:\tjava org.apache.lucene.demo.SearchFiles [-index dir] [-field f] [-repeat n] [-queries file] [-query string] [-raw] [-paging hitsPerPage]\n";
         if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
@@ -48,5 +66,52 @@ public class SearchFiles {
                 i++;
             }
         }
+
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
+        IndexSearcher searcher = new IndexSearcher(reader);
+        Analyzer analyzer = new StandardAnalyzer();
+
+        BufferedReader in = null;
+        if (queries != null) {
+            in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
+        } else {
+            in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        }
+        QueryParser parser = new QueryParser(field, analyzer);
+        while (true) {
+            if (queries == null && queryString == null) {                        // prompt the user
+                System.out.println("Enter query: ");
+            }
+
+            String line = queryString != null ? queryString : in.readLine();
+
+            if (line == null || line.length() == -1) {
+                break;
+            }
+
+            line = line.trim();
+            if (line.length() == 0) {
+                break;
+            }
+
+            Query query = parser.parse(line);
+            System.out.println("Searching for: " + query.toString(field));
+
+            if (repeat > 0) {                           // repeat & time as benchmark
+                Date start = new Date();
+                for (int i = 0; i < repeat; i++) {
+                    searcher.search(query, 100);
+                }
+                Date end = new Date();
+                System.out.println("Time: " + (end.getTime() - start.getTime()) + "ms");
+            }
+
+            //doPagingSearch();
+
+            if (queryString != null) {
+                break;
+            }
+        }
+        reader.close();
     }
 }
