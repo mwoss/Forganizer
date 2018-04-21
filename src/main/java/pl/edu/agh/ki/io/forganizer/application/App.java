ded870c2736868e19e1719e5fd54ea763ad97380
Mateusz Woś <wos.mateusz16@gmail.com>
2018-04-21 14:21:47 +0200

    Partial stage handle done

--- src/main/java/pl/edu/agh/ki/io/forganizer/application/App.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/application/App.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import pl.edu.agh.ki.io.forganizer.view.MainWindowView;
 
-public class Main extends Application{
+public class App extends Application{
     public static void main(String[] args) {
         launch(args);
     }
@@ -13,5 +13,7 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception {
         MainWindowView mainWindowView = new MainWindowView(primaryStage);
         mainWindowView.showStage();
+//        TestView testView = new TestView(primaryStage);
+//        testView.showStage();
     }
 }
--- src/main/java/pl/edu/agh/ki/io/forganizer/presenter/MainWindowController.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/presenter/MainWindowController.java
@@ -1,28 +1,26 @@
 package pl.edu.agh.ki.io.forganizer.presenter;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 
-public class Controller {
+public class MainWindowController {
     @FXML
     private BorderPane mainView;
 
     @FXML
     private void handleChangeView(ActionEvent event) {
         try {
-            String menuItemID = ((MenuItem) event.getSource()).getId();
-
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(menuItemID + ".fxml"));
+            String menuItemID = ((JFXButton) event.getSource()).getId();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/fxml/" + menuItemID + ".fxml"));
             loader.setController(this);
 
             mainView.setCenter(loader.load());
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
