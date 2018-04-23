18c64676905cebeb391b2748f11caac21e63dded
GillinedUp <myrreah@gmail.com>
2018-04-23 18:50:33 +0200

    added morfologik dependency

--- src/main/java/pl/edu/agh/ki/io/forganizer/search/Indexer.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/search/Indexer.java
@@ -1,4 +1,26 @@
 package pl.edu.agh.ki.io.forganizer.search;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.morfologik.MorfologikAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
 public class Indexer {
+
+    private String indexPath = "index";
+    private boolean updateIndex = true;
+    private Analyzer analyzer;
+
+    public Indexer(String indexPath, String analyzerLanguage) {
+        this.indexPath = indexPath;
+        if (analyzerLanguage.equals("english")) {
+            analyzer = new StandardAnalyzer();
+        } else if (analyzerLanguage.equals("polish")) {
+            analyzer = new MorfologikAnalyzer();
+        }
+    }
+
+    public void addFile(String fileName, String filePath) {
+
+
+    }
 }
