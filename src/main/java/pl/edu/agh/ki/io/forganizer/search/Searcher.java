80455772322beb42bac4b760b38cbd7fc0eaf14a
GillinedUp <myrreah@gmail.com>
2018-04-24 22:41:49 +0200

    finished search

--- src/main/java/pl/edu/agh/ki/io/forganizer/search/Searcher.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/search/Searcher.java
@@ -1,4 +1,46 @@
 package pl.edu.agh.ki.io.forganizer.search;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.morfologik.MorfologikAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.FSDirectory;
+
+import java.nio.file.Paths;
+
 public class Searcher {
+
+    private String indexPath;
+    private Analyzer analyzer;
+
+    public Searcher(String indexPath, String analyzerLanguage) {
+        this.indexPath = indexPath;
+        if (analyzerLanguage.equals("english")) {
+            analyzer = new StandardAnalyzer();
+        } else if (analyzerLanguage.equals("polish")) {
+            analyzer = new MorfologikAnalyzer();
+        }
+    }
+
+    public Document[] searchField(String fieldName,
+                                  String queryString,
+                                  int resultsNum) throws Exception {
+        IndexReader indexReader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+        QueryParser queryParser = new QueryParser(fieldName, analyzer);
+        Query query = queryParser.parse(queryString);
+        ScoreDoc[] hits = indexSearcher.search(query, resultsNum).scoreDocs;
+        Document[] results = new Document[hits.length];
+        for (int i = 0; i < hits.length; i++) {
+            results[i] = indexSearcher.doc(hits[i].doc);
+        }
+        return results;
+    }
+
 }
