3bbf5982861b0ec6687e6e48a1e10cd0e091986b
GillinedUp <myrreah@gmail.com>
2018-05-06 22:15:39 +0200

    added IndexerTest.java

---
+++ src/test/java/pl/edu/agh/ki/io/forganizer/IndexerTest.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.ki.io.forganizer;
+
+import org.junit.jupiter.api.Test;
+import pl.edu.agh.ki.io.forganizer.search.Indexer;
+
+public class IndexerTest {
+
+    @Test
+    void addFileTest() {
+        Indexer indexer = new Indexer("test", "english");
+
+    }
+
+}
