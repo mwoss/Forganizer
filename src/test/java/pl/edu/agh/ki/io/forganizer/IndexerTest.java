08f44a5313d3592510ee9f93cd04574b34a7f1ee
GillinedUp <myrreah@gmail.com>
2018-05-07 21:15:26 +0200

    reformating addFile

--- src/test/java/pl/edu/agh/ki/io/forganizer/IndexerTest.java
+++ src/test/java/pl/edu/agh/ki/io/forganizer/IndexerTest.java
@@ -1,16 +1,24 @@
 package pl.edu.agh.ki.io.forganizer;
 
 import org.junit.jupiter.api.Test;
+import pl.edu.agh.ki.io.forganizer.search.FolderType;
 import pl.edu.agh.ki.io.forganizer.search.Indexer;
+import pl.edu.agh.ki.io.forganizer.search.Language;
+
+import java.io.IOException;
 
 public class IndexerTest {
 
     @Test
     void addFileTest() {
-        Indexer indexer = new Indexer("test", "english");
+        Indexer indexer = new Indexer("test", Language.ENGLISH);
         try {
-            indexer.addFile("file1.txt", "/home/yurii/tmp/file1.txt");
-        } catch (Exception ex) {
+            indexer.addFile(
+                    "file1.txt",
+                    "/home/yurii/tmp/file1.txt",
+                    FolderType.RAM);
+
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
