f5522d42e6f893fa22e5b24a9078274e9484c311
Anna Marciniec <annamarciniec24601@gmail.com>
2018-05-09 18:12:56 +0200

    added file2doc converting

--- src/main/java/pl/edu/agh/ki/io/forganizer/model/Converter.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/model/Converter.java
@@ -1,6 +1,8 @@
 package pl.edu.agh.ki.io.forganizer.model;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
 
 public class Converter {
 
@@ -13,4 +15,12 @@ public class Converter {
         return file;
     }
 
+    public Document convertFileToDoc(File file) {
+        Document document = new Document();
+        document.add(new StringField("name", file.getName(), Field.Store.YES));
+        document.add(new StringField("path", file.getPath(), Field.Store.YES));
+        if (file.getTag() != null) document.add(new StringField("tag", file.getTag(), Field.Store.YES));
+        if (file.getComment() != null) document.add(new StringField("comment", file.getComment(), Field.Store.YES));
+        return document;
+    }
 }
