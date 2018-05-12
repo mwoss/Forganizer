bfa62ae5408d765bfc57a14756489127bda5caa5
Anna Marciniec <annamarciniec24601@gmail.com>
2018-05-12 15:20:38 +0200

    implemented addFile in FileManager

--- src/main/java/pl/edu/agh/ki/io/forganizer/model/FileManager.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/model/FileManager.java
@@ -1,27 +1,36 @@
 package pl.edu.agh.ki.io.forganizer.model;
 
+import pl.edu.agh.ki.io.forganizer.search.FolderType;
 import pl.edu.agh.ki.io.forganizer.search.IndexFiles;
+import pl.edu.agh.ki.io.forganizer.search.Indexer;
+import pl.edu.agh.ki.io.forganizer.search.Language;
 
+import java.io.IOException;
 import java.util.List;
 
 public class FileManager {
-    IndexFiles indexFiles;
 
-    public FileManager() {
+    private Indexer indexer;
+    private Converter converter;
+
+    public FileManager(String path,Language language) {
+        indexer = new Indexer(path, language);
+        converter = new Converter();
     }
 
-    public void addFile(File file){
+    public void addFile(File file, FolderType folderType) throws IOException {
+        indexer.addFile(converter.convertFileToDoc(file), folderType);
+    }
+
+    public void updateFile(File file) {
 
     }
 
-    public void updateFile(File file){
-
-    }
-    public void removeFile(File file){
+    public void removeFile(File file) {
 
     }
 
-    List<File> getAllFiles(){
+    List<File> getAllFiles() {
 
         return null;
     }
