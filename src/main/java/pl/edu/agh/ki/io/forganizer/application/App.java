b66c9b07c746d74a0c17a19ec96804b0b86a60b6
GillinedUp <myrreah@gmail.com>
2018-06-09 21:06:50 +0200

    add setIndexPath to SettingsManager

--- src/main/java/pl/edu/agh/ki/io/forganizer/application/App.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/application/App.java
@@ -2,15 +2,9 @@ package pl.edu.agh.ki.io.forganizer.application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import pl.edu.agh.ki.io.forganizer.model.FileManager;
-import pl.edu.agh.ki.io.forganizer.search.Language;
 import pl.edu.agh.ki.io.forganizer.utils.Const;
+import pl.edu.agh.ki.io.forganizer.utils.SettingsManager;
 import pl.edu.agh.ki.io.forganizer.view.MainWindowView;
-
-import java.io.IOException;
-import java.nio.file.Paths;
 
 public class App extends Application{
     public static void main(String[] args) {
@@ -19,6 +13,8 @@ public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        SettingsManager settingsManager = new SettingsManager();
+        settingsManager.setIndexFolder();
         MainWindowView mainWindowView = new MainWindowView(primaryStage);
         mainWindowView.showStage();
     }
--- src/main/java/pl/edu/agh/ki/io/forganizer/utils/SizeConverter.java
+++ src/main/java/pl/edu/agh/ki/io/forganizer/utils/SizeConverter.java
@@ -1,14 +1,14 @@
 package pl.edu.agh.ki.io.forganizer.utils;
 
-public final class  SizeConverter {
+public final class SizeConverter {
 
-    public static String getReadableFileSize(Long fileSize){
+    public static String getReadableFileSize(Long fileSize) {
         int unit = -1;
-        do{
+        do {
             fileSize /= 1024;
             unit++;
-        }while (fileSize > 1024);
-        String size = String.format("%.1f",Math.max(fileSize, 0.1));
+        } while (fileSize > 1024);
+        String size = String.format("%.1f", Math.max(fileSize, 0.1));
         return size + Const.unitsArray[unit];
     }
 }
